8ccd1b838913786f94b4e5d65ae62d64b2ad1447
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-19 10:17:02 -0300

    s3a47: finish button components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import './Button.css'
 
-export default props => {
-    return <button className="button">{props.label}</button>
-}
+export default props => 
+     <button 
+     onClick={e => props.click && props.click(props.label)}
+     className={`
+        button 
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+     `}>
+        {props.label}
+     </button>
